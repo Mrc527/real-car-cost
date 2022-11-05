a5a8137dc73f143ccbd0bc39ea96410a3621b922
Marco Visin <marco@visin.ch>
2022-11-05 16:57:14 +0100

    save step

--- src/components/buystep.js
+++ src/components/buystep.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+
 import 'antd/dist/antd.css';
 //import './index.css';
 import { PlusOutlined } from '@ant-design/icons';
@@ -21,24 +22,40 @@ const { RangePicker } = DatePicker;
 const { TextArea } = Input;
 
 const BuyStep = () => {
-  const [componentDisabled, setComponentDisabled] = useState(true);
-  const onFormLayoutChange = ( disabled ) => {
-    console.log('disabled', disabled)
-    setComponentDisabled(disabled);
-  };
 
+  const [buyData, setBuyData] = useState(() => {
+    // getting stored value
+    const saved = localStorage.getItem("buyData");
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+  const [form] = Form.useForm();
+  console.log("buyData",buyData)
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem("buyData", JSON.stringify(buyData));
+  }, [buyData]);
   return (
     <>
-
       <Form
+        form={form}
+        initialValues={buyData}
         labelCol={{ span: 4 }}
         wrapperCol={{ span: 14 }}
         layout="horizontal"
-        onValuesChange={onFormLayoutChange}
-        onFieldsChange={onFormLayoutChange}
-        preserve={true}
+        onValuesChange={() => {
+
+          setBuyData(form.getFieldsValue())
+        }}
+        onFinish={(asd) => {
+          console.log("finish",asd)
+        }}
+        preserve
       >
-        <Form.Item label="Car Type">
+        <Form.Item preserve name={["app"]}>
+          <Input />
+        </Form.Item>
+        <Form.Item preserve name={["car_type"]} label="Car Type">
           <Select>
             <Select.Option value="city-car">City Car</Select.Option>
             <Select.Option value="standard">Standard</Select.Option>
@@ -47,10 +64,10 @@ const BuyStep = () => {
             <Select.Option value="offroad">Off-road / Pickup</Select.Option>
           </Select>
         </Form.Item>
-        <Form.Item label="Buy Date">
+        <Form.Item preserve name={["buy_date"]} label="Buy Date">
           <DatePicker />
         </Form.Item>
-        <Form.Item label="Buy Price">
+        <Form.Item preserve name={["buy_price"]} label="Buy Price">
           <InputNumber />
         </Form.Item>
         <Form.Item label="TextArea">
